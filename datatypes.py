5b8920f7b8c276a9fcdeeeff68e46332f23431e6
Arjun Ajith <arjunbkallara@gmail.com>
2025-11-02 18:35:40 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,12 @@
+#Creating variables with different data types
+a=7
+b=9.5
+c="Christmas"
+d=8+6j
+e=[a,b,c]
+f=(1,2,3,4)
+g={"Cat","Mouse"}
+#printing all the variables in a list
+print([a,b,c,d,e,f,g])
+#printing all the datatypes as a list
+print([type(a),type(b),type(c),type(d),type(e),type(f),type(g)])
